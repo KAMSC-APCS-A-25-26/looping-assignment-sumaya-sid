0cec357f1e478eff396c275c59a90477a39377ae
sumaya-sid <sumaya.siddiqui49024@gmail.com>
2025-10-13 13:42:38 -0400

    Update CharacterPerLine.java

--- src/main/java/CharacterPerLine.java
+++ src/main/java/CharacterPerLine.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class CharacterPerLine {
     public static void main(String[] args) {        
         // TODO: Step 1 - Get user input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
 
         // TODO: Step 2 - Print each character on a separate line
+        int num = word.length();
+        int k = 0;
+        for (int j=1; j<num+1; j++)
+        {
+            System.out.println(word.substring(k,j));
+            k++;
+        }
 
     }
 }
