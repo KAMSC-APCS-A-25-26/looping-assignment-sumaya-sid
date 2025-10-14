c469e8124c83ff51c19bac36653bb1cc39a17945
sumaya-sid <sumaya.siddiqui49024@gmail.com>
2025-10-14 13:35:59 -0400

    Update NumberTrianglePattern.java

--- src/main/java/NumberTrianglePattern.java
+++ src/main/java/NumberTrianglePattern.java
@@ -9,20 +9,17 @@ public class NumberTrianglePattern {
         int num = sc.nextInt();
 
         // TODO: Step 2 - Generate the number triangle pattern
-        for (int j=1; j<num+1; j++)
-        {
-            for (int k=1; k<=j; k++)
-            {
-                if (j!=k)
-                {
-                    System.out.print(k+" ");
+        if (num>0) {
+            for (int j = 1; j < num + 1; j++) {
+                for (int k = 1; k <= j; k++) {
+                    if (j != k) {
+                        System.out.print(k + " ");
+                    } else {
+                        System.out.print(k);
+                    }
                 }
-                else {
-                    System.out.print(k);
-                }
+                System.out.println();
             }
-            System.out.println();
         }
-
     }
 }
